8d4227e921768f1b00b11f7f8018bcc2bf7415a4
Jyoti Ranjan Sharma <appdeveloperubuntu@gmail.com>
2020-12-26 08:09:06 +0530

    Project usinh JSTL

---
+++ src/com/demo/jstl/DemoServlet.java
@@ -0,0 +1,23 @@
+package com.demo.jstl;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/DemoServlet")
+public class DemoServlet extends HttpServlet {
+	
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		String name = "Krishna";
+		
+		req.setAttribute("label", name);
+		RequestDispatcher rd = req.getRequestDispatcher("display.jsp");
+		rd.forward(req, res);
+	}
+	
+}
