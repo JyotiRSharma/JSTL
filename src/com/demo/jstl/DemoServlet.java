aa5d8319ef9d01437cd230aa8216d804863d5fbb
Jyoti Ranjan Sharma <appdeveloperubuntu@gmail.com>
2020-12-26 20:16:41 +0530

    Using a list of Students
    Adding 3 students to the list

--- src/com/demo/jstl/DemoServlet.java
+++ src/com/demo/jstl/DemoServlet.java
@@ -14,8 +14,14 @@ public class DemoServlet extends HttpServlet {
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		Student s = new Student("Jyoti", 12);
+		Student s1 = new Student("Ram", 10);
+		Student s2 = new Student("Shyam", 20);
+		List<Student> studs = new ArrayList<Student>();
+		studs.add(s);
+		studs.add(s1);
+		studs.add(s2);
 		
-		req.setAttribute("student", s);
+		req.setAttribute("student", studs);
 		RequestDispatcher rd = req.getRequestDispatcher("display.jsp");
 		rd.forward(req, res);
 	}
